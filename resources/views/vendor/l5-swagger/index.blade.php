f755e8ea5ffc78bb690abaab39ffa619ce497880
unknown <hasyim.asyari.js@gmail.com>
2022-02-11 07:16:25 +0700

    fix link assets

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -1,71 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>{{config('l5-swagger.documentations.'.$documentation.'.api.title')}}</title>
+    <title>{{ config('l5-swagger.documentations.' . $documentation . '.api.title') }}</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('docs/asset/swagger-ui.css') }}">
-    <link rel="icon" type="image/png" href="{{  asset('docs/asset/favicon-32x32.png') }}" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="{{  asset('docs/asset/favicon-16x16.png') }}" sizes="16x16"/>
+    <link rel="icon" type="image/png" href="{{ asset('docs/asset/favicon-32x32.png') }}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ asset('docs/asset/favicon-16x16.png') }}" sizes="16x16" />
     <style>
-    html
-    {
-        box-sizing: border-box;
-        overflow: -moz-scrollbars-vertical;
-        overflow-y: scroll;
-    }
-    *,
-    *:before,
-    *:after
-    {
-        box-sizing: inherit;
-    }
+        html {
+            box-sizing: border-box;
+            overflow: -moz-scrollbars-vertical;
+            overflow-y: scroll;
+        }
 
-    body {
-      margin:0;
-      background: #fafafa;
-    }
+        *,
+        *:before,
+        *:after {
+            box-sizing: inherit;
+        }
+
+        body {
+            margin: 0;
+            background: #fafafa;
+        }
+
     </style>
 </head>
 
 <body>
-<div id="swagger-ui"></div>
+    <div id="swagger-ui"></div>
 
-<script src="{{ asset('docs/asset/swagger-ui-bundle.js') }}"></script>
-<script src="{{ asset('docs/asset/swagger-ui-standalone-preset.js') }}"></script>
-<script>
-    window.onload = function() {
-        // Build a system
-        const ui = SwaggerUIBundle({
-            dom_id: '#swagger-ui',
-            url: "{!! $urlToDocs !!}",
-            operationsSorter: {!! isset($operationsSorter) ? '"' . $operationsSorter . '"' : 'null' !!},
-            configUrl: {!! isset($configUrl) ? '"' . $configUrl . '"' : 'null' !!},
-            validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
-            oauth2RedirectUrl: "{{ route('l5-swagger.'.$documentation.'.oauth2_callback', [], $useAbsolutePath) }}",
+    <script src="{{ asset('docs/asset/swagger-ui-bundle.js') }}"></script>
+    <script src="{{ asset('docs/asset/swagger-ui-standalone-preset.js') }}"></script>
+    <script>
+        window.onload = function() {
+            // Build a system
+            const ui = SwaggerUIBundle({
+                dom_id: '#swagger-ui',
+                url: "{!! $urlToDocs !!}",
+                operationsSorter: {!! isset($operationsSorter) ? '"' . $operationsSorter . '"' : 'null' !!},
+                configUrl: {!! isset($configUrl) ? '"' . $configUrl . '"' : 'null' !!},
+                validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
+                oauth2RedirectUrl: "{{ route('l5-swagger.' . $documentation . '.oauth2_callback', [], $useAbsolutePath) }}",
 
-            requestInterceptor: function(request) {
-                request.headers['X-CSRF-TOKEN'] = '{{ csrf_token() }}';
-                return request;
-            },
+                requestInterceptor: function(request) {
+                    request.headers['X-CSRF-TOKEN'] = '{{ csrf_token() }}';
+                    return request;
+                },
 
-            presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIStandalonePreset
-            ],
+                presets: [
+                    SwaggerUIBundle.presets.apis,
+                    SwaggerUIStandalonePreset
+                ],
 
-            plugins: [
-                SwaggerUIBundle.plugins.DownloadUrl
-            ],
+                plugins: [
+                    SwaggerUIBundle.plugins.DownloadUrl
+                ],
 
-            layout: "StandaloneLayout",
-            docExpansion : "{!! config('l5-swagger.defaults.ui.display.doc_expansion', 'none') !!}",
-            filter: {!! config('l5-swagger.defaults.ui.display.filter') ? 'true' : 'false' !!},
-            persistAuthorization: "{!! config('l5-swagger.defaults.ui.authorization.persist_authorization') ? 'true' : 'false' !!}",
+                layout: "StandaloneLayout",
+                docExpansion: "{!! config('l5-swagger.defaults.ui.display.doc_expansion', 'none') !!}",
+                filter: {!! config('l5-swagger.defaults.ui.display.filter') ? 'true' : 'false' !!},
+                persistAuthorization: "{!! config('l5-swagger.defaults.ui.authorization.persist_authorization') ? 'true' : 'false' !!}",
 
-        })
+            })
 
-        window.ui = ui
-    }
-</script>
+            window.ui = ui
+        }
+    </script>
 </body>
+
 </html>
